eb8c65bf05721f25a8df8ec7aa0b4696858ff7a4
Aparna Anil <aparna.anil@msf.ad>
2021-11-24 12:55:29 +0530

    first commit

---
+++ src/components/data/Constants.js
@@ -0,0 +1,9 @@
+export const data = {
+  PLANT_PRICE: "Price ",
+  PLANT_LIGHT: "Light ",
+  PLANT_WATERING: "Watering ",
+  PLANT_MAINTENANCE: "Maintenance ",
+  PLANT_WHERETOGROW: "Position ",
+  ADD_TO_CART: "Add To Cart",
+  CAPTION: "Plant Planet",
+};
